acf7b72302dc12fe33814979395c9fc34054e222
李少龙 <2718893665@qq.com>
2024-07-01 09:37:56 +0800

    这是第一次提交

---
+++ src/main/java/com/lzy/gitdemo001/GitDemo001Application.java
@@ -0,0 +1,13 @@
+package com.lzy.gitdemo001;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitDemo001Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GitDemo001Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/lzy/gitdemo001/GitDemo001ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lzy.gitdemo001;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitDemo001ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
